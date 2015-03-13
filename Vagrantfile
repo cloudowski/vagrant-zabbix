4261acae16a453f67afcc12164020b6b701181cc
Tomasz Cholewa <slashroot@slashroot.eu>
2015-03-13 20:22:10 +0100

    Still sux - keep on trying

--- Vagrantfile
+++ Vagrantfile
@@ -10,7 +10,7 @@ VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   
-  config.vm.box = "puppetlabs/centos-6.5-64-puppet"
+  config.vm.box = "puppetlabs/centos-6.6-64-puppet"
     
 #  config.vm.box = "puppetlabs/ubuntu-14.04-64-puppet"
     if Vagrant.has_plugin?("vagrant-cachier")
--- puppet/modules/profile/manifests/zabbix/server.pp
+++ puppet/modules/profile/manifests/zabbix/server.pp
@@ -1,15 +1,18 @@
-class profile::zabbix::server {
+class profile::zabbix::server(
+    $zabbix_version = '2.4'
+
+) {
 
     class { 'apache':
         mpm_module => 'prefork',
     }
-#    include apache
+
     include apache::mod::php
 
     class { 'postgresql::server': }
-    class { 'zabbix': }
+    class { 'zabbix': zabbix_version => $zabbix_version }
 #    class { 'zabbix::proxy': }
-    class { 'zabbix::agent': }
+    class { 'zabbix::agent': zabbix_version => $zabbix_version }
                
 
 }
